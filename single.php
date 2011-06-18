8a42f9e713e8b011c2c68e05922640a2a5a98ed2
John Louis del Rosario <john2x@gmail.com>
2011-06-18 23:59:35 +0800

    First commit.

---
+++ single.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+
+			<div class="entry-content">
+				
+				<?php the_content(); ?>
+
+				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+				
+				<?php the_tags( 'Tags: ', ', ', ''); ?>
+			
+				<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+
+				<div class="entry-background">
+					<?php 
+					 if ( has_post_thumbnail()) {
+					   $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large');
+					   echo '<img src="' . $large_image_url[0] . '"/>';
+					 }
+					?>
+				</div>
+			</div>
+			
+			<?php edit_post_link('Edit this entry','','.'); ?>
+			
+		</article>
+
+	<?php comments_template(); ?>
+
+	<?php endwhile; endif; ?>
+	
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
