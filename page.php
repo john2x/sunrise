09f8fd4cd6d67aa04dbf0550232c361446783d41
John Louis del Rosario <john2x@gmail.com>
2011-06-21 01:22:04 +0800

    Styled date in posts. Changed markup in page.php.

--- page.php
+++ page.php
@@ -4,27 +4,29 @@
 			
 		<article class="post" id="post-<?php the_ID(); ?>">
 
-			<h2><?php the_title(); ?></h2>
+			<div class="entry-content">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
 
-			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+				<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
 
-			<div class="entry">
+				<!--<div class="entry">-->
 
-				<?php the_content(); ?>
+					<?php the_content(); ?>
 
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 
+				<!--</div>-->
+
+				<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 			</div>
-
-			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 
 		</article>
 		
-		<?php comments_template(); ?>
+		<?php //comments_template(); ?>
 
 		<?php endwhile; endif; ?>
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -4,10 +4,10 @@
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-
 			<div class="entry-content">
 				
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+
 				<?php the_content(); ?>
 
 				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
@@ -34,7 +34,7 @@
 
 	<?php endwhile; endif; ?>
 	
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 
 </div>
 <?php get_footer(); ?>
