33c072108b3198c58ca421e053ac421ac9bc4751
John Louis del Rosario <john2x@gmail.com>
2011-06-22 03:43:49 +0800

    Oops.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div id="body">
+<div class="body">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
