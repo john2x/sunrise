0dc2c23940bf536ce1e2d57a2952e71f035a717a
John Louis del Rosario <john2x@gmail.com>
2011-06-22 05:30:35 +0800

    Start on index.

--- index.php
+++ index.php
@@ -8,14 +8,16 @@
 
 			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
 
+			<!--
 			<div class="entry">
 				<?php the_content(); ?>
 			</div>
+			-->
 
 			<footer class="postmetadata">
 				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php //comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+				<!--Posted in <?php the_category(', ') ?> | 
+				<?php //comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>-->
 			</footer>
 
 		</article>
