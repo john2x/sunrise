21f71e19bc875d6fd556f7490e814a07e9cf98b0
John Louis del Rosario <john2x@gmail.com>
2011-07-06 11:32:39 +0800

    Thumbnail branch. Test.
    --HG--
    branch : thumbnail

--- index.php
+++ index.php
@@ -4,19 +4,24 @@
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-			<h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			<!--<h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>-->
 
-			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+			<?php //include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
 
 			<div class="content-wrapper">
-				<?php the_content(); ?>
+				<?php //the_content(); ?>
+				<?php if ( has_post_thumbnail()) {
+				   $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail');
+				   echo '<img class=".thumbnail" src="' . $large_image_url[0] . '"/>';
+				 }
+				?>
 			</div>
 
-			<footer class="postmetadata">
+			<!--<footer class="postmetadata">
 				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				<!--Posted in <?php the_category(', ') ?> | 
-				<?php //comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>-->
-			</footer>
+				Posted in <?php the_category(', ') ?> | 
+				<?php //comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+			</footer>-->
 
 		</article>
 
